6e7a9612bff34bf7fe685f37393e25f329b4e0c9
ZIDIKHERY MCHOMVU <101670659+LXXOIII@users.noreply.github.com>
2022-10-07 14:58:59 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import './App.css'
 import GitHub from './GitHub'
-// import UserForm from './UserForm'
 
 function App() {
   return (
     <>
-      {/* <UserForm /> */}
       <GitHub />
     </>
   )
